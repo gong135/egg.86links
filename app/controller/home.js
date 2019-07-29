54c542b1aa264d1cdb29892186e641af7896f140
龚小敏 <xmgong@86links.com>
2019-07-29 20:29:19 +0800

    #

--- app/controller/home.js
+++ app/controller/home.js
@@ -4,8 +4,9 @@ const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
   async index() {
-    const client = await this.app.mysql.get('db1');
-    const user = await client.query('users');
+    // const client = await this.app.mysql.get('db1');
+    // const user = await client.query('users');
+    const user = await this.app.mysql.query('select * from users', '');
     console.log(user);
     this.ctx.body = 'hi, egg';
   }
